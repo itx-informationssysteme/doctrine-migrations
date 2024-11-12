34577ad32b95370e8d6f2204805ff052305cad92
pmy <patrik.marosvary@itx.de>
2024-11-12 09:04:52 +0100

    chore: Updated README.md

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,5 +2,6 @@ CREATE TABLE doctrine_migration_versions
 (
     version         varchar(191) NOT NULL,
     executed_at     datetime,
-    execution_time  int(11)
+    execution_time  int(11),
+    PRIMARY KEY (version)
 );
